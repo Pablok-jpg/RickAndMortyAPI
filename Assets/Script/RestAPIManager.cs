a7e2dd7c1a0d522742eceda2affd9902766065ba
Pablo <pablo.catanor@upb.edu.co>
2023-02-18 00:51:34 -0500

    final

--- Assets/Script/RestAPIManager.cs
+++ Assets/Script/RestAPIManager.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
-using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -9,140 +7,103 @@ using UnityEngine.UI;
 public class RestAPIManager : MonoBehaviour
 {
     [SerializeField] private List<RawImage> YourRawImage;
-    [SerializeField] public List<TextMeshProUGUI> textNames;
 
-    [SerializeField] private int userID = 1;
+    private string RickAndoMortyApi = "https://rickandmortyapi.com/api";
+    private string ServerApiPath = "https://my-json-server.typicode.com/Pablok-jpg/RickAndMortyAPI";
+
     private List<int> generatedNumbers = new List<int>();
     public int[] cards;
+    private int UserId = 1;
 
-    [SerializeField] private string myApiPath = "https://my-json-server.typicode.com/Pablok-jpg/RickAndMortyAPI";
-    [SerializeField] private string rickApiPath = "https://rickandmortyapi.com/api/character";
-    void Start()
+    public void GetPlayerDataOnClick()
     {
-
+        UserId = Random.Range(1, 6);
+        StartCoroutine(GetPlayerData());
     }
 
-    public void GetPlayerInfoClick()
+    public void RandomID()
     {
-        userID = Random.Range(1, 6);
-        StartCoroutine(GetPlayerInfo());
+        
+        Debug.Log("el id es:" + UserId);
+        StartCoroutine(GetPlayerData());
     }
 
-    public void IDRandomizer(int number)
+    IEnumerator GetPlayerData()
     {
-        StartCoroutine(GetPlayerInfo());
-    }
-
-
-    IEnumerator GetPlayerInfo()
-    {
-        UnityWebRequest www = UnityWebRequest.Get(myApiPath + "/users/" + userID);
+        string url = ServerApiPath + "/users/" + UserId;
+        Debug.Log(url);
+        UnityWebRequest www = UnityWebRequest.Get(url);
         yield return www.Send();
 
         if (www.isNetworkError)
         {
-            Debug.Log("NETWORK ERROR: " + www.error);
+            Debug.Log("NETWORK ERROR:" + www.error);
         }
         else
         {
-
             if (www.responseCode == 200)
             {
-                string json = www.downloadHandler.text;
-                JsonData user = JsonUtility.FromJson<JsonData>(json);
-
-                Debug.Log("Username:" + user.name);
-                Debug.Log("User ID:" + user.id);
-
+                UserDataJSON user = JsonUtility.FromJson<UserDataJSON>(www.downloadHandler.text);
                 for (int i = 0; i < user.deck.Length; i++)
                 {
-                    StartCoroutine(GetCharacters(user.deck[i], i));
+                    StartCoroutine(GetCharacter(user.deck[i], i));
                     yield return new WaitForSeconds(0.1f);
                 }
-
-            }
-
-            else if (www.responseCode == 404)
-            {
-                Debug.Log("Character not found!");
             }
             else
             {
-                string mensaje = "Status: " + www.responseCode;
-                mensaje += "\ncontent-type: " + www.GetResponseHeader("content-type");
-                mensaje += "\nError : " + www.error;
+                string mensaje = "Status:" + www.responseCode;
+                mensaje += "\ncontent-type:" + www.GetResponseHeader("content-type");
+                mensaje += "\nError:" + www.error;
                 Debug.Log(mensaje);
             }
-
-            byte[] results = www.downloadHandler.data;
         }
     }
-    IEnumerator GetCharacters(int ID, int place)
+    IEnumerator GetCharacter(int Id, int Place)
     {
-        UnityWebRequest www = UnityWebRequest.Get(rickApiPath + ID);
-        Debug.Log(www);
+        UnityWebRequest www = UnityWebRequest.Get(RickAndoMortyApi + "/character/" + Id);
         yield return www.Send();
 
         if (www.isNetworkError)
         {
-            Debug.Log("NETWORK ERROR: " + www.error);
+            Debug.Log("NETWORK ERROR:" + www.error);
         }
         else
         {
-            //Debug.Log(www.GetResponseHeader("content-type"));
-
-            // Show results as text
-            Debug.Log(www.downloadHandler.text);
-
             if (www.responseCode == 200)
             {
-                string jsonb = www.downloadHandler.text;
-                Character character = JsonUtility.FromJson<Character>(jsonb);
-
-                Debug.Log("Name : " + character.name);
-
-                textNames[place].text = character.name;
-                StartCoroutine(DownloadImage(character.image, place));
-            }
-            else if (www.responseCode == 404)
-            {
-                Debug.Log("Character not found!");
+                Character character = JsonUtility.FromJson<Character>(www.downloadHandler.text);
+                Debug.Log(character.id);
+                StartCoroutine(DownloadImage(character.image, Place));
             }
             else
             {
-                string mensaje = "Status: " + www.responseCode;
-                mensaje += "\ncontent-type: " + www.GetResponseHeader("content-type");
-                mensaje += "\nError : " + www.error;
+                string mensaje = "Status:" + www.responseCode;
+                mensaje += "\ncontent-type:" + www.GetResponseHeader("content-type");
+                mensaje += "\nError:" + www.error;
                 Debug.Log(mensaje);
-
             }
-
-            byte[] results = www.downloadHandler.data;
         }
     }
-    IEnumerator DownloadImage(string MediaUrl, int place)
+
+    IEnumerator DownloadImage(string url, int Place)
     {
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
         yield return request.SendWebRequest();
 
-        if (request.isNetworkError || request.isHttpError) Debug.Log(request.error);
-        else YourRawImage[place].texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        if (request.isNetworkError || request.isHttpError)
+            Debug.Log(request.error);
+        else YourRawImage[Place].texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
     }
 }
+
 [System.Serializable]
 public class Character
 {
-    public string name;
-    public string species;
+    public int id;
     public string image;
 }
-
-[System.Serializable]
-public class JsonData
+public class UserDataJSON
 {
-    public int id;
-    public string name;
     public int[] deck;
 }
-
-
